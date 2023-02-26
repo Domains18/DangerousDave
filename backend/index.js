2930555a1e8d1b524ae3921d327f99df3fdc03e2
Domains18 <gibsonsgibson88@gmail.com>
2023-02-26 15:38:11 +0300

    backend

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,8 @@ const database = mysql.createConnection({
     password: 'password',
     database: 'test'
 });
-
-
-
-
 const app = express();
+app.use(express.json());
 
 
 
@@ -23,9 +20,21 @@ app.get('/api/hello', (req, res) => {
 app.get('/api/books', (req, res) => {
     const query = 'SELECT * FROM books';
     database.query(query, (err, results) => {
-        err?  res.send(err)  : res.send(results);
+        err ? res.send(err) : res.send(results);
     });
 });
 
+app.post("/api/books", (req, res) => {
+    const query = 'INSERT INTO books (`title`, `desc`, `cover`) VALUES(?)';
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ]
+
+    database.query(query, [values], (err, results) => {
+        err ? res.send(err) : res.json("Book added successfully");
+    });
+});
 
 app.listen(5000, () => console.log('Listening on port 5000'));
