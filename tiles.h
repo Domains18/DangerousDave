0b93d7878f3007d2b4af5ee51cdc9d4f5b4dff8c
Domains18 <gibsonsgibson88@gmail.com>
2026-04-02 07:39:20 +0300

    feat: implement tile definitions and level 1 map layout

---
+++ tiles.h
@@ -0,0 +1,17 @@
+#ifndef TILES_H
+#define TILES_H
+
+#define TILE_SIZE 32
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 15
+
+
+#define EMPTY 0
+#define WALL 1
+#define SPIKE 2
+#define TROPHY 3
+
+
+extern int level1_map[MAP_HEIGHT][MAP_WIDTH];
+
+#endif
